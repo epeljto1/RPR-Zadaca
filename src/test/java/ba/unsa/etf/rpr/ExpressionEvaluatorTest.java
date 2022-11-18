f779a32d037e2eb8bdb1cf97177b92f2fa1a55bd
epeljto1 <epeljto1@etf.unsa.ba>
2022-11-18 18:12:02 +0100

    initial commit

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import junit.framework.Assert;
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class ExpressionEvaluatorTest
 {
     @Test
-    public void evaluateTest1() {
+    void evaluateTest1() {
         String s1 = new String("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )");
         double a = ExpressionEvaluator.evaluate(s1);
         double e = 101;
@@ -16,7 +17,7 @@ public class ExpressionEvaluatorTest
     }
 
     @Test
-    public void evaluateTest2()
+    void evaluateTest2()
     {
         String s2 = new String("( 5 - ( ( 25 - 5 ) / ( 2 * 2 ) ) )");
         double a = ExpressionEvaluator.evaluate(s2);
@@ -25,11 +26,34 @@ public class ExpressionEvaluatorTest
     }
 
     @Test
-    public void evaluateTest3()
+    void evaluateTest3()
     {
         String s3 = new String("( ( ( ( 100 * 100 ) - ( 99 * 100 ) ) / 50 ) * sqrt ( 100 ) )");
         double a = ExpressionEvaluator.evaluate(s3);
         double e = 20;
         Assert.assertEquals(e,a);
+    }
+
+    @Test
+    void evaluateTest4()
+    {
+        String s4 = new String("( ( ( ( ( 1 + 3 ) - 3 ) * 10 ) / 0.1 ) * sqrt ( 121 ) )");
+        double a = ExpressionEvaluator.evaluate(s4);
+        double e = 1100;
+        Assert.assertEquals(e,a);
+    }
+
+    @Test
+    void evaluateTest5()
+    {
+        String s5 = new String("( abc + ( 5 * 10 ) )");
+        assertThrows(RuntimeException.class, () -> ExpressionEvaluator.evaluate(s5));
+    }
+
+    @Test
+    void evaluateTest6()
+    {
+        String s6 = new String("1+(2+3)");
+        assertThrows(RuntimeException.class, () -> ExpressionEvaluator.evaluate(s6));
     }
 }
